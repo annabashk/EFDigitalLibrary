5735d091d4a76ca288e6403312b22f450a2cbb82
Анна Башкатова <annabashk13@gmail.com>
2023-04-24 21:47:37 +0300

    Добавьте файлы проекта.

---
+++ EFDigitalLibrary/AppContext.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace EFDigitalLibrary
+{
+    public class AppContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Book> Books { get; set; }
+
+        public AppContext()
+        {
+            Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source=LAPTOP-ELM1M63U;Database=DigitalLibrary;Trusted_Connection=True; TrustServerCertificate=True");
+        }
+    }
+}
---
+++ EFDigitalLibrary/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace EFDigitalLibrary
+{ 
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var db = new AppContext())
+            {
+                var user1 = new User { Name = "Анна", Email = "anna@mail.ru" };
+
+                db.Users.Add(user1);
+
+                var book1 = new Book { Name = "Идиот", Year = "1821" };
+
+                db.Books.Add(book1);
+
+                db.SaveChanges();
+            }
+        }
+    }
+}
